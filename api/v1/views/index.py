ef15b353cfecb74b379cf9ceb71af2c2159cfaaf
Matias Acosta <2277@holbertonschool.com>
2021-05-06 17:26:44 -0300

    push mati

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,29 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
 def status():
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats')
+def stats():
+    new_dict = {
+    "amenities": Amenity, 
+    "cities": City, 
+    "places": Place, 
+    "reviews": Review, 
+    "states": State, 
+    "users": User
+    }
+    for key, value in new_dict.items():
+        new_dict[key] = storage.count(value)
+    return jsonify(new_dict)
