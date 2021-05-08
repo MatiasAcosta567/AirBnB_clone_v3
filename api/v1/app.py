21a9d76eabb6d447b2b817b30f3d3e97b5ad77f0
Matias Acosta <2277@holbertonschool.com>
2021-05-08 17:16:04 -0300

    Task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,7 @@ from flask_cors import CORS
 from models import storage
 from os import getenv
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
