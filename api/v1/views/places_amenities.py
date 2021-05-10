883ad18726d70d18afcda3ff194adfe47cc2d8c3
Matias Acosta <2277@holbertonschool.com>
2021-05-10 13:44:25 -0300

    Test

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -5,7 +5,6 @@ from os import getenv
 from api.v1.views import app_views
 from flask import jsonify, request, abort, make_response
 from models import storage
-from models.review import Review
 from models.place import Place
 from models.user import User
 from models.amenity import Amenity
@@ -15,14 +14,18 @@ from models import storage_t
 @app_views.route('/places/<place_id>/amenities', methods=['GET'],
                  strict_slashes=False)
 def get_amenities(place_id):
-    place = storage.get(Place, place_id)
-    if place is not None:
-        if storage_t == 'db':
-            return jsonify([amenity.to_dict() for amenity in place.amenities])
-        else:
-            return jsonify([amenity.to_dict() for amenity in
-                           place.amenity_ids])
-    abort(404)
+    """get amenity information for a specified place"""
+    place = storage.get("Place", place_id)
+    if place is None:
+        abort(404)
+    amenities = []
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        amenity_objects = place.amenities
+    else:
+        amenity_objects = place.amenity_ids
+    for amenity in amenity_objects:
+        amenities.append(amenity.to_dict())
+    return jsonify(amenities)
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
